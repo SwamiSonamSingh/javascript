885feac33fa5a52387d9ed1a0472b68e848c911b
SwamiSonamSingh <swamisonamkumarsingh576@gmail.com>
2021-05-19 12:49:32 +0530

    j1

---
+++ script.js
@@ -0,0 +1,6 @@
+var p=document.getElementById("btnbtn");
+p.addEventListener("click",clicking);
+function clicking(){
+    document.getElementById("content").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea saepe voluptates, odio molestias distinctio culpa praesentium harum vero corporis animi";
+    document.getElementById("head").style="display:flex;flex-direction:row;justify-content:center;"
+}
